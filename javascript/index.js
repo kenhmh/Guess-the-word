e11396bfbaa195ceb0b12356d4637b916f93ac7a
kenhmh <92768141+kenhmh@users.noreply.github.com>
2022-02-16 11:27:45 +0800

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -14,6 +14,16 @@ const createNewWords = () => {
     console.log(newTempSwords)
     return newTempSwords;
 }
+const scrambleWords = (arr) =>{
+    for (let i = arr.length-1; i>0; i--){
+        let temp = arr[i];
+        let j = Math.floor(Math.random()*(i+1));
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        return arr;
+    }
+}
 
 btn.addEventListener('click', function(){
     if(!play){
